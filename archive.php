66836b0bad81c8a4b79de0d0a08a209466593ab7
elvina <theelvina@gmail.com>
2016-01-09 18:40:18 +0000

    remove search from mobile version on category pages

--- archive.php
+++ archive.php
@@ -5,7 +5,7 @@
 
 if ( is_post_type_archive('game') || is_tax() ) {
     ?>
-   <div class="homesearch"><div class="text-hide"><h3>Ask the Guru</h3></div>
+   <div class="homesearch hidden-xs"><div class="text-hide"><h3>Ask the Guru</h3></div>
 		<?php 
 		get_template_part('templates/searchform', 'game'); 
 		get_template_part('templates/searchform', 'filter'); 
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'sage'); ?>
   </div>
+  
   <?php get_search_form(); ?>
 <?php endif; ?>
 
